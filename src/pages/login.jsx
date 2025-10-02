a1a211fe9aa9f24b48014f6e1e379824cafbd0b9
Zeiyar <marieycorentin97@gmail.com>
2025-10-02 15:16:23 +0200

    new series db from serv

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import bgVideo from "../image/Space-Run-Art-GIF-by-PERFECTL00P.mp4"
+import vidBg from "../image/Running-Man-Space-GIF.mp4"
 
 function Login (){
     const [email,setEmail] = useState("");
@@ -35,12 +36,14 @@ function Login (){
     return(
         <>
         {loading && (<video className="loadingGIF" autoPlay muted loop src={bgVideo} type="video/mp4"/>)}
-        <form onSubmit={handleSubmit} className="login">
+        <div className="login">
+            <video src={vidBg} autoPlay muted loop type="video/mp4"/>
+        <form onSubmit={handleSubmit} >
         <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} placeholder="doulbi@gmail.com" required/>
         <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="type your password here..." required/>
         <button type="submit">Lez goooo</button>
         <button type="button" onClick={()=>navigate("/register")}>Register?</button>
-        </form></>
+        </form></div></>
     )
 };
 
