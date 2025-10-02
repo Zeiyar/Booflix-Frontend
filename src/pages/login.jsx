70ce905cb9aeda0ec548cc2c8ea3a262ac78078c
Zeiyar <marieycorentin97@gmail.com>
2025-10-02 18:56:11 +0200

    finishing

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import bgVideo from "../image/Space-Run-Art-GIF-by-PERFECTL00P.mp4";
 import vidBg from "../image/Running-Man-Space-GIF.mp4";
 
-
 function Login (){
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
@@ -51,8 +50,8 @@ function Login (){
             autoPlay muted loop src={bgVideo} type="video/mp4"/>
         )}
         <div className="login">
-            <video src={vidBg} autoPlay muted loop type="video/mp4" className="vidbg"/>
             <form onSubmit={handleSubmit} >
+                <video src={vidBg} autoPlay muted loop type="video/mp4" className="vidbg"/>
                 <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} placeholder="doulbi@gmail.com" required/>
                 <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="type your password here..." required/>
                 <button type="submit">Lez goooo</button>
