a998ce46a9c560fa0b9568ba9dc1a449c02e1938
Zeiyar <marieycorentin97@gmail.com>
2025-10-10 13:23:45 +0200

    adding one space for debog

--- src/pages/params.jsx
+++ src/pages/params.jsx
@@ -218,7 +218,7 @@ function Params (){
                     {message && <p>{message}</p>}
             </form>
         </section>
-        <button style={{background: "red",height:"50px",width:"100px"}} onClick={logout}>Déconnexion</button>
+        <button style={{background: "red",height:"50px",width:"150px"}} onClick={logout}>Déconnexion</button>
         </main>
     )
 }
